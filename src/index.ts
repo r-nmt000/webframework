f9b24260edc31f05be8605c404a97ee484f9b5b4
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-07-20 23:37:01 +0900

    add: on and trigger

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,15 @@ const user = new User({name: 'name', age: 20});
 
 user.set({name: 'new name'});
 
+user.on('click', () => {
+    console.log('click');
+});
+
+user.on('click', () => {
+    console.log('click2');
+});
+
+user.trigger('click');
+
 console.log(user.get('name'));
 console.log(user.get('age'));
