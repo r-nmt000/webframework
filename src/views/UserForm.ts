dffdeebc53704deec8f91670087db800cf221e85
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-07-25 16:46:18 +0900

    add: eventhander

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -5,11 +5,33 @@ export class UserForm {
         this.parent = parent;
     }
 
+    eventsMap(): {[key:string]: () => void } {
+        return {
+            'click:button': this.onButtonClick
+        }
+    }
+
+    onButtonClick(): void {
+        console.log('click!');
+    }
+
+    bindEvents(fragment: DocumentFragment): void {
+        const eventsMap = this.eventsMap();
+
+        for (let eventKey in eventsMap) {
+            const [eventName, selector] = eventKey.split(':');
+            fragment.querySelectorAll(selector).forEach(element => {
+                element.addEventListener(eventName, eventsMap[eventKey]);
+            })
+        }
+
+    }
+
     template(): string {
         return `
           <div>
             <h1>User Form</h1>
-            <input />
+            <input /><button>click me</button>
           </div>
         `;
     }
@@ -18,6 +40,7 @@ export class UserForm {
         const templateElement = document.createElement('template');
         templateElement.innerHTML = this.template();
 
+        this.bindEvents(templateElement.content);
         this.parent.append(templateElement.content);
     }
 }
